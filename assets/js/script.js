26be783f5a80d21f44f045409b0f1b2e08a64d52
muhammadalikhurbonov <muhammadaliqurbonov551@gmail.com>
2023-06-06 13:12:07 +0500

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,24 +5,3 @@ function modalFunction() {
   document.querySelector(".section_geroy_modal").classList.toggle("scale");
 }
 
-// const swiper = new Swiper('.swiper', {
-//   // Optional parameters
-//   direction: 'horizontal',
-//   loop: true,
-
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: '.swiper-scrollbar',
-//   },
-// });
