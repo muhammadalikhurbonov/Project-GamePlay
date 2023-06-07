7fb53444cebeac030baaefda351dcc6f05ea5c8a
muhammadalikhurbonov <muhammadaliqurbonov551@gmail.com>
2023-06-07 18:02:26 +0500

    COMMIT

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,24 @@
 "use strict";
 
-
 function modalFunction() {
   document.querySelector(".section_geroy_modal").classList.toggle("scale");
 }
 
+var acc = document.getElementsByClassName("section_servise_main_item");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var accordion = this.nextElementSibling;
+    if (accordion.style.display === "block") {
+      accordion.style.display = "none";
+    } else {
+      accordion.style.display = "block";
+    }
+  });
+}
