78cb9e8bd439c5d4725af4e6e75f2e8886fabcba
muhammadalikhurbonov <muhammadaliqurbonov551@gmail.com>
2023-06-10 10:08:23 +0500

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,51 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// var swiper = new Swiper(".MYSwiper", {
+//   slidesPerView: 4,
+//   centeredSlides:true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     type: "fraction",
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+// var appendNumber = 4;
+// var prependNumber = 1;
+// document
+//   .querySelector(".prepend-2-slides")
+//   .addEventListener("click", function (e) {
+//     e.preventDefault();
+//     swiper.prependSlide([
+//       '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+//       '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+//     ]);
+//   });
+// document
+//   .querySelector(".prepend-slide")
+//   .addEventListener("click", function (e) {
+//     e.preventDefault();
+//     swiper.prependSlide(
+//       '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
+//     );
+//   });
+// document.querySelector(".append-slide").addEventListener("click", function (e) {
+//   e.preventDefault();
+//   swiper.appendSlide(
+//     '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
+//   );
+// });
+// document
+//   .querySelector(".append-2-slides")
+//   .addEventListener("click", function (e) {
+//     e.preventDefault();
+//     swiper.appendSlide([
+//       '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+//       '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+//     ]);
+//   });
