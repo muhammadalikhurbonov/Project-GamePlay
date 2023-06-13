60889326b423e4ab24f3229d3e380369741d739c
muhammadalikhurbonov <muhammadaliqurbonov551@gmail.com>
2023-06-13 10:14:48 +0500

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,28 +79,3 @@ var swiper = new Swiper(".MYSwiper", {
 //     },
 //   },
 // });
-
-// const progressCircle = document.querySelector(".autoplay-progress svg");
-// const progressContent = document.querySelector(".autoplay-progress span");
-// var swiper = new Swiper(".MYSwiper", {
-//   spaceBetween: 30,
-//   centeredSlides: true,
-//   autoplay: {
-//     delay: 2500,
-//     disableOnInteraction: false
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev"
-//   },
-//   on: {
-//     autoplayTimeLeft(s, time, progress) {
-//       progressCircle.style.setProperty("--progress", 1 - progress);
-//       progressContent.textContent = `${Math.ceil(time / 1000)}s`;
-//     }
-//   }
-// });
